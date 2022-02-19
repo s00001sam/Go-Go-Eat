3df71ebb021444a094f11568967af579585c8af0
s00001sam <s00001sam@gmail.com>
2022-02-19 18:17:21 +0800

    renew PriceAdapter

--- app/src/main/java/com/sam/gogoeat/view/search/PriceAdapter.kt
+++ app/src/main/java/com/sam/gogoeat/view/search/PriceAdapter.kt
@@ -3,25 +3,58 @@ package com.sam.gogoeat.view.search
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
 import com.sam.gogoeat.databinding.ItemPricePopupBinding
-import com.sam.gogoeat.view.support.BaseBindingAdapter
 
-class PriceAdapter : BaseBindingAdapter<String, ItemPricePopupBinding>() {
+class PriceAdapter() : ListAdapter<String, PriceAdapter.ViewHolder>(DiffCallback) {
 
     private var currentPosition: Int = 0
     private var itemClickListener: ((position: Int)->Unit)? = null
 
-    override fun bindView(binding: ItemPricePopupBinding, position: Int, data: String) {
-        binding.s = data
-        binding.tvPrice.isSelected = position == currentPosition
-        binding.root.setOnClickListener {
-            itemClickListener?.invoke(position)
-            setCurrentPosition(position)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder.from(parent)
+    }
+
+
+    class ViewHolder(private val binding: ItemPricePopupBinding, private val context: Context) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(price: String, position: Int, currentPosition: Int) {
+            binding.s = price
+            binding.tvPrice.isSelected = position == currentPosition
+
+            binding.executePendingBindings()
+        }
+
+        companion object {
+            fun from(parent: ViewGroup): ViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = ItemPricePopupBinding.inflate(layoutInflater, parent, false)
+                return ViewHolder(binding, parent.context)
+            }
         }
     }
 
-    override fun createBinding(context: Context, parent: ViewGroup): ItemPricePopupBinding {
-        return ItemPricePopupBinding.inflate(LayoutInflater.from(context), parent, false)
+    companion object DiffCallback : DiffUtil.ItemCallback<String>() {
+        override fun areItemsTheSame(oldItem: String, newItem: String): Boolean {
+            return oldItem === newItem
+        }
+
+        override fun areContentsTheSame(oldItem: String, newItem: String): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val price = getItem(position)
+
+        holder.itemView.setOnClickListener {
+            itemClickListener?.invoke(position)
+            setCurrentPosition(position)
+        }
+
+        holder.bind(price, position, currentPosition)
     }
 
     fun setCurrentPosition(position: Int) {
